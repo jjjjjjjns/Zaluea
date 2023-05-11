55bf2486a2b585675e0d2fac6cc493e7b50fde9c
jjjjjjjns <63760073+jjjjjjjns@users.noreply.github.com>
2023-05-11 22:17:42 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,35 +2,62 @@ import Server from 'bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
 
-const bare = new Server('/bare/', '');
+const PASSWORD = 'mysecretpassword';
+
+const bare =  new Server('/bare/', '');
 const serve = new nodeStatic.Server('Site/');
 const server = http.createServer();
 
-// Set the password to "mypassword"
-const PASSWORD = 'mypassword';
-
+// Handle HTTP requests
 server.on('request', (request, response) => {
-    // Check if the user has provided the correct password
-    if (request.headers.authorization !== `Basic ${Buffer.from(`:${PASSWORD}`).toString('base64')}`) {
-        response.writeHead(403);
-        response.end('Access denied');
-        return;
-    }
+  // If the password is correct, set a cookie and allow access to the site
+  if (request.url === '/login' && request.method === 'POST') {
+    let body = '';
+    request.on('data', chunk => {
+      body += chunk.toString();
+    });
+    request.on('end', () => {
+      const params = new URLSearchParams(body);
+      if (params.get('password') === PASSWORD) {
+        response.writeHead(302, {
+          'Set-Cookie': `Access=granted; Max-Age=${8*60*60}`,
+          'Location': '/'
+        });
+        response.end();
+      } else {
+        response.writeHead(401, {'Content-Type': 'text/plain'});
+        response.write('Invalid password');
+        response.end();
+      }
+    });
+    return;
+  }
 
-    if (bare.route_request(request, response)) return true;
+  // If the access cookie is set, allow access to the site
+  const cookie = request.headers.cookie;
+  if (cookie && cookie.indexOf('Access=granted') !== -1) {
     serve.serve(request, response);
+  } else {
+    // Otherwise, prompt for the password
+    response.writeHead(401, {'Content-Type': 'text/html'});
+    response.write(`
+      <html>
+        <body>
+          <form method="POST" action="/login">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" value="Login">
+          </form>
+        </body>
+      </html>
+    `);
+    response.end();
+  }
 });
 
+// Handle WebSocket upgrades
 server.on('upgrade', (req, socket, head) => {
-    // Check if the user has provided the correct password
-    if (req.headers.authorization !== `Basic ${Buffer.from(`:${PASSWORD}`).toString('base64')}`) {
-        socket.write('HTTP/1.1 403 Access denied\r\n\r\n');
-        socket.end();
-        return;
-    }
-
-    if (bare.route_upgrade(req, socket, head)) return;
-    socket.end();
+	if(bare.route_upgrade(req, socket, head))return;
+	socket.end();
 });
 
 server.listen(process.env.PORT || 8080);
