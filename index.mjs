167a42342d53a3617c7e1f2fbec24bc27087f90f
jjjjjjjns <63760073+jjjjjjjns@users.noreply.github.com>
2023-05-11 22:37:30 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -7,34 +7,32 @@ const serve = new nodeStatic.Server('Site/');
 const server = http.createServer();
 
 server.on('request', (request, response) => {
-  if (bare.route_request(request, response)) return true;
-  if (checkAuthCookie(request)) {
-    serve.serve(request, response);
-  } else {
-    response.writeHead(401, { 'Content-Type': 'text/plain' });
-    response.end('Your Request Was UnAuthed By The TeamBlatant Security System');
-  }
+    const cookies = parseCookies(request);
+    const authCookie = cookies.Auth;
+    if (authCookie === '1273174891') {
+        // Serve requested content
+        serve.serve(request, response);
+    } else {
+        // Unauthorized access, return 401 Unauthorized status code
+        response.writeHead(401);
+        response.end();
+    }
 });
-
-
 
 server.on('upgrade', (req, socket, head) => {
-  if (bare.route_upgrade(req, socket, head)) return;
-  socket.end();
+    if (bare.route_upgrade(req, socket, head)) return;
+    socket.end();
 });
 
-server.listen(process.env.PORT || 8080);
-
-function checkAuthCookie(request) {
-  const cookies = request.headers.cookie;
-  if (cookies) {
-    const authCookie = cookies.split(';').find(cookie => cookie.trim().startsWith('AUTH='));
-    if (authCookie) {
-      const authValue = authCookie.split('=')[1];
-      if (authValue === '12313134') {
-        return true;
-      }
+function parseCookies(request) {
+    const cookies = {};
+    if (!request.headers.cookie) {
+        return cookies;
     }
-  }
-  return false;
+    const cookiesRaw = request.headers.cookie.split(';');
+    cookiesRaw.forEach(cookieRaw => {
+        const [name, value] = cookieRaw.split('=').map(c => c.trim());
+        cookies[name] = value;
+    });
+    return cookies;
 }
